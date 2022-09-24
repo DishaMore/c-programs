f07431a9fb61a8def7df092087a6a75f3b94135f
DishaMore <110332623+DishaMore@users.noreply.github.com>
2022-09-24 13:09:04 +0530

    Add files via upload

---
+++ conditional control statements/Mall_ if else.c
@@ -0,0 +1,26 @@
+// Mall _ if else //
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  float Amount = 0.0;
+  printf("\n enter your Account balance=");
+  scanf("%f",&Amount);
+  
+  if(Amount > 1000)
+  {
+    printf("\n \n \t welcome to mall !!\n");
+  }
+  else
+  {
+    printf("\n \n \t minimum balance required to shop is 1000+\n");
+    
+   
+  }
+  printf("\n \n Thanks for visit!!");
+  
+  getch();
+   
+  
+}  
+ 
