cd883ceb21c879178c494e762340ea380b736f8b
DishaMore <110332623+DishaMore@users.noreply.github.com>
2022-09-29 21:44:12 +0530

    Add files via upload

---
+++ conditional control statements/For.c
@@ -0,0 +1,17 @@
+// For //
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int i = 0;
+  for(i=1;i<5;i++)
+  {
+   printf("\n \n indea");
+   printf("\n i love my indea");
+  }
+ printf("\n \n thanks");
+ getch();
+}
+   
+   
+  
