7545633d28f4f56f5ca1737465486573d77bf3d7
DishaMore <110332623+DishaMore@users.noreply.github.com>
2022-09-29 21:44:58 +0530

    Add files via upload

---
+++ program to print table of given number-for.c
@@ -0,0 +1,30 @@
+//program to print table of given number - for//
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int No=0,cnt=0;
+  printf("\n enter a number ot print its table");
+  scanf("%d",&No);
+  
+  for(cnt=1; cnt<=10;cnt++)
+  {
+    printf("%d",&No,&cnt);
+  }
+printf("\n\n tahnks");
+getch();
+}
+
+
+  
+      
+      
+  
+***=====\n");
+  printf("\n\n thanks");
+  getch();
+}
+  
+      
+      
+  
