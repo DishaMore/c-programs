adba6099cbbf66401a0a23eca086ecc7f9f87d1b
DishaMore <110332623+DishaMore@users.noreply.github.com>
2022-10-02 12:26:49 +0530

    Add files via upload

---
+++ conditional control statements/do while.c
@@ -0,0 +1,19 @@
+// do while //
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int i = 1;
+  do
+  {
+    printf("\n indea");
+    printf("\n value of variable",i);
+  }  
+  
+ while(i<=5);
+ 
+   printf("\n \n thanks");
+ getch();
+}
+  
+ 
