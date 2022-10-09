b012c1f4b91053090eec3b5eb2f4e8e1ed09b2cf
DishaMore <110332623+DishaMore@users.noreply.github.com>
2022-10-09 14:35:36 +0530

    Add files via upload

---
+++ conditional control statements/positive or negative numbers.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int Num=0;
+  printf("\n enter two numbers=");
+  scanf("%d",&Num);
+if(Num>0)
+{
+  printf("\n %d is positive",Num);
+  
+}
+else
+{
+  printf("\n %d is negative",Num);
+}
+printf("\n thanks");
+getch();
+}
