36c1a369deb4e2915840f96f49bc43cf796e2dc1
DishaMore <110332623+DishaMore@users.noreply.github.com>
2022-10-15 11:42:57 +0530

    Add files via upload

--- conditional control statements/program to calculate sum of digits in given numbers.c
+++ conditional control statements/program to calculate sum of digits in given numbers.c
@@ -2,23 +2,24 @@
 #include<conio.h>
 int main()
 {
-  int No=0, Dig=0, dsum=0, temp=0;
+  int No=0, dcnt=0, temp=0;
   up:
-  printf("\n enter positive integer Number to calculate its Digit sum=");
+  printf("\n enter positive integer Number to calculate its digit sum=");
   scanf("%d",&No);
-  
   temp=No;
-  if(No<0)
+if(No<0)
   {
-    printf("\n Invalide Input...");
+    printf("\n Invalid Input....");
     goto up;
   }
-  while(temp>0)
-  {
-    Dig=temp%10;
-    dsum=dsum+Dig;
-    temp=temp/10;
-  }
-  printf("\n \n sum of Digits in given number %d is=%d",No,dsum);
+while(temp>0)
+{
+  dcnt++;
+  temp/10;
+} 
+printf("\n \n No of digit in Given Number %d is = %d",No,dcnt);
+getch();
+}
+  ts in given number %d is=%d",No,dsum);
   getch();
 }
