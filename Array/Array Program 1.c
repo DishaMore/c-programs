e93aef3f863a2e8b8aaf363fdee2b68bc9f79a66
DishaMore <110332623+DishaMore@users.noreply.github.com>
2022-10-18 07:25:20 +0530

    Add files via upload

---
+++ Array/Array Program 1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int i=0;
+  int Marks[5];
+  Marks[0]=80;
+  Marks[1]=70;
+  Marks[2]=60;
+  Marks[3]=50;
+  Marks[4]=40;
+  
+for(i=0;i<5;i++)
+{
+  printf("%d \n",Marks[i]);
+}
+getch();
+}
