8ff09bf941785591c5640954819020bf34f39878
DishaMore <110332623+DishaMore@users.noreply.github.com>
2022-11-03 22:22:43 +0530

    Add files via upload

---
+++ String/string program 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  char Arr[50]={};
+  puts("\n Enter a String :");
+  gets(Arr);
+  puts("\n Give string is =>");
+  puts(Arr);
+  
+ getch(); 
+}
+
