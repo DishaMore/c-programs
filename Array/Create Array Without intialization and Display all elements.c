5c52f035399088183256d8c8fd9a67dc2231417f
DishaMore <110332623+DishaMore@users.noreply.github.com>
2022-11-07 23:33:03 +0530

    Add files via upload

---
+++ Array/Create Array Without intialization and Display all elements.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int Arr[7];
+printf("\n the 1st element is = %d",Arr[0]);
+printf("\n the 2nd element is = %d",Arr[1]);
+printf("\n the 3rd element is = %d",Arr[2]);
+printf("\n the 4th element is = %d",Arr[3]);
+printf("\n the 5th element is = %d",Arr[4]);
+printf("\n the 6th element is = %d",Arr[5]);
+printf("\n the 7th element is = %d",Arr[6]);
+
+printf("\n \n Thanks !!!");
+
+getch();
+}
