0bc27ee0f3ba73ccb71c669650b56f3c25adfcac
DishaMore <110332623+DishaMore@users.noreply.github.com>
2022-11-07 09:27:58 +0530

    Add files via upload

---
+++ pattern printing/Program to print all element from A to Z or a to z.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  char i;
+  printf("small letters: \n");
+  for(i='a';i<='z';i++)
+  {
+    printf("%c",i);
+  }
+  printf("\n \n Capital letters: \n");
+  for(i='A';i<='Z';i++)
+  {
+    printf("%c",i);
+  }
+ getch();
+}
+
