9fea5534c677b3529ebd668668ab4f2e183a8bab
DishaMore <110332623+DishaMore@users.noreply.github.com>
2023-03-16 21:52:44 +0530

    Add files via upload

---
+++ Pointer/Basic Pointer.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+  int No = 45;
+  
+  int *ptr = (&No);
+  
+  printf("\n  value of No = %d",No);
+  printf("\n Address of No = %d",&No);
+  printf("\n value of ptr = %d",No);
+  printf("\n Address of ptr = %d",&No);
+  
+  getch();
+  
+}  
+  
+  
+  
+  
+  
